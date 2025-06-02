a670b0835dd25a6ce85b8e8b43bb167e5ca742c4
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2025-06-02 21:03:46 +0800

    feat: make the newslatter template dynamic

--- index.ts
+++ index.ts
@@ -12,13 +12,40 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const article = [
+  {
+    "id": 44129480,
+    "title": "A man who sailed round the world with a chicken (2019)",
+    "url": "https://www.theguardian.com/global/2019/apr/21/why-did-the-chicken-cross-the-globe-french-sailor-guirec-soudee-monique",
+    "description": "Meet Monique, for the past five years, she and Guirec Soudée have been sailing around the world. Emma Beddington reveals how the French sailor chose to brave the high seas with just one feathered shipmate",
+    "image": "https://i.guim.co.uk/img/media/dabe8105841641e22106f62494bdec3406cb8cf2/0_1993_4912_2948/master/4912.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdG8tYWdlLTIwMTkucG5n&enable=upscale&s=3af8de3f306c5a8f0e3bd0c54080ddd4",
+    "favicon": null
+  },
+  {
+    "id": 44155635,
+    "title": "LFSR CPU Running Forth",
+    "url": "https://github.com/howerj/lfsr-vhdl",
+    "description": "An implementation of a CPU that uses a Linear Feedback Shift Register as a Program Counter instead of a normal one - howerj/lfsr-vhdl",
+    "image": "https://opengraph.githubassets.com/a8982acfc39b6dc0bedf406cfe5ef4237ab31c3e7ff86d34013991dbb860e43c/howerj/lfsr-vhdl",
+    "favicon": null
+  },
+  {
+    "id": 44142359,
+    "title": "Writing your own C++ standard library part 2",
+    "url": "https://nibblestew.blogspot.com/2025/05/writing-your-own-c-standard-library.html",
+    "description": "This blog post  talked about the \"self written C++ standard library\" I wrote for the fun of it ( code here ). The post got linked by Hackern...",
+    "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjHnzalf3ku3pnatODqLs6qwd3fRsKIfpTTOTuV2O6Jaw4ceN_ISzLOna_7W-5xSGTX7g-CYLVrW4kbFIndCeJwfoYYI-j-qJ4bFhjxCse1Od4Pqa2JimIiPkejj-8YY8ebBE1fp6fTbAY-T3iMKQhwSMAfhm41qOrEM79dPgqB0rqP_ex7Fupagyp_bfw/w1200-h630-p-k-no-nu/Screenshot%20From%202025-05-02%2022-49-16.png",
+    "favicon": null
+  },
+];
+
 (async () => {
   const info = await transporter.sendMail({
     from: '"Test Message" <techglimpse.email@gmail.com>',
     to: "odeardika.app@gmail.com",
     subject: "Hello ✔",
     text: "Hello world?", // plain‑text body
-    html: emailTemplate(), // HTML body
+    html: emailTemplate(article), // HTML body
   });
 
   console.log("Message sent:", info.messageId);
--- template/newsMessage.ts
+++ template/newsMessage.ts
@@ -1,6 +1,81 @@
-import fs from "fs";
-
-export default function template () {
-    const htmlString = fs.readFileSync("./index.html", "utf-8");
-    return htmlString;
+export default function template (article : {
+    id: number;
+    title: string;
+    url: string;
+    description: string;
+    image: string;
+    favicon: null;
+}[]) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Newsletter Email</title>
+</head>
+<body style="margin:0; padding:0; background:#fff;">
+    <table width="100%" bgcolor="#fff" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td align="center">
+                <table width="600" cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff" style="margin:40px auto; padding:24px; border-collapse:collapse;">
+                    <tr>
+                        <td align="center" style="font-size:24px; font-weight:bold; color:#2d3a4a; font-family:Arial, sans-serif; padding-bottom:4px;">
+                            Tech Glimpse
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" style="color:#4a5a6a; font-family:Arial, sans-serif; font-size:16px; padding-bottom:3rem;">
+                            Stay updated with the latest technology news every day
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="font-family:Arial, sans-serif; font-size:16px; color:#444444; padding-right: 1.5rem; padding-left: 1.5rem;">
+                            <!-- Article 1 -->
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border:1px solid #cccccc;border-radius: 1rem; margin-bottom:16px;">
+                                <tr>
+                                    <td style="padding:16px;">
+                                        <h2 style="font-size:18px; margin:0 0 8px 0; color:#2d3a4a; font-family:Arial, sans-serif;">${article[0].title}</h2>
+                                        <img src="${article[0].image}" alt="Article's Image" style="display:block; max-width:100%; height:auto; margin:0 0 12px 0;">
+                                        <p style="margin:0; color:#444444;">${article[0].description}</p>
+                                        <span style="color:#444444;">For full article <a href="${article[0].url}" style="margin-top:1px; text-decoration:none; color:#7d7d7d">Click Here</a></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- Article 2 -->
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border:1px solid #cccccc; border-radius: 1rem; margin-bottom:16px;">
+                                <tr>
+                                    <td style="padding:16px;">
+                                        <h2 style="font-size:18px; margin:0 0 8px 0; color:#2d3a4a; font-family:Arial, sans-serif;">${article[1].title}</h2>
+                                        <img src="${article[1].image}" alt="Article's Image" style="display:block; max-width:100%; height:auto; margin:0 0 12px 0;">
+                                        <p style="margin:0; color:#444444;">${article[1].description}</p>
+                                        <span style="color:#444444;">For full article <a href="${article[1].url}" style="margin-top:1px; text-decoration:none; color:#7d7d7d">Click Here</a></span>
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- Article 3 -->
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border:1px solid #cccccc; border-radius: 1rem; margin-bottom:16px;">
+                                <tr>
+                                    <td style="padding:16px;">
+                                        <h2 style="font-size:18px; margin:0 0 8px 0; color:#2d3a4a; font-family:Arial, sans-serif;">${article[2].title}</h2>
+                                        <img src="${article[2].image}" alt="Article's Image" style="display:block; max-width:100%; height:auto; margin:0 0 12px 0;">
+                                        <p style="margin:0; color:#444444;">${article[2].description}</p>
+                                        <span style="color:#444444;">For full article <a href="${article[2].url}" style="margin-top:1px; text-decoration:none; color:#7d7d7d">Click Here</a></span>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" style="font-size:12px; color:#888888; font-family:Arial, sans-serif; padding-top:2rem; padding-bottom:2rem;">
+                            &copy; 2025 Ode Ardika. All rights reserved.<br>
+                            If you want more news <a href="https://tech-glimpse.odeardika.my.id/" style="color:#888888;">click here</a>.
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>    
+`
 }
